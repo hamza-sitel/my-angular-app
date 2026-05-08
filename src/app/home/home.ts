fd02aa5e21255d7aa1aa38d58132a5eadca5f85a
hamza <hsitel424@gmail.com>
2026-05-08 20:14:58 +0100

    improve ui styling for food app (header ,search , cards) and creat commpent name search and connect with others and input search is working now everything is working

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,20 +3,33 @@ import { RouterModule } from '@angular/router';
 import {Food} from '../services/food/food'
 import { CommonModule } from '@angular/common';
 import {FOOD} from '../shared/models/food'
+import { ActivatedRoute } from '@angular/router';
+import {Search} from '../search/search'
 
 
 @Component({
   selector: 'app-home',
   standalone:true,
-  imports: [RouterModule ,CommonModule,],
+  imports: [RouterModule ,CommonModule ,Search],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home implements OnInit {
   foods:FOOD[]=[]
-  constructor(private foodService:Food){}
+  constructor(private foodService:Food , private route : ActivatedRoute){}
   ngOnInit():void{
-    this.foods =this.foodService.getAll();
+      this.route.params.subscribe(params =>{
+        if(params['searchTerm'])
+          this.foods= this.foodService.getAll().filter(food => {
+            
+           return  food.name.toLowerCase().includes(params['searchTerm'].toLowerCase())
+          })
+        else
+                this.foods =this.foodService.getAll();
+            
+
+          
+      })
   }
 
   getStarsArray(stars: number): number[] {
